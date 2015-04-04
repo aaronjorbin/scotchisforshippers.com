70bb9e585c6461cb391482b46d033b98fde174d4
Aaron Jorbin <aaron@jorb.in>
2015-04-04 03:52:00 +0000

    initial commit

---
+++ js/sifs.bbq.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	$('#yes').click(function() {
+		$('#main').replaceWith('<h2 id="win">Then drink some damn scotch</h2><p id="post-yes-tweet" ><a  class="addthis_button_twitter" style="cursor:pointer"</a>');
+		_gaq.push(['_trackEvent', 'shippers', 'win', 'Scotch Is For BBQ Shippers']);	
+		var twitter_share = 
+		{	
+			templates: {
+				twitter: 'I shipped BBQ today. Did you? {{url}}'
+			}
+		};
+
+		addthis.toolbox( '#post-yes-tweet', twitter_share, twitter_share);
+		$('#post-yes-tweet .addthis_button_twitter').html('Tell the world you shipped BBQ');
+
+	});
+	$('#no').click(function() {
+		$('#main').replaceWith('<h2 id="lose">Then get the smoker going so you can drink some damn scotch</h2>');
+		_gaq.push(['_trackEvent', 'shippers', 'lose', 'Scotch Is For BBQ Shippers']);	
+	});
+
+	var docHieght = $(document).height();
+	var base = 42;
+	var fontSize =  (docHieght -  (docHieght % base )) / base  ;
+	if (fontSize == 0)
+		fontSize = 22;
+	$('body').css('font-size', fontSize); 
+
+
+	});
